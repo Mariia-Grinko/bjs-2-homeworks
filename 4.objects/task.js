1f879dadfb19ee10579b9c96598b66ad45d35a0c
Mariia Grinko <mariiapryashnikova@gmail.com>
2021-08-10 12:26:39 +0300

    done homework4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+}
+const studentP = new Student("Petrov");
+const studentS = new Student("Sidorov");
+const studentI = new Student("Ivanov");
 
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+}
+Student.prototype.addMark = function(mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+Student.prototype.addMarks = function(...rest) {
+    for (let i = 0; i < rest.length; i++) {
+        this.addMark(rest[i]);
+    }
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.getAverage = function() {
+    let sum = 0;
+    sum = marks.map((element) => { return sum + element; });
+    return sum / this.marks.length;
 }
 
-// ваш код для остальных методов
+Student.prototype.exclude = function(reason) {
+    delete Student.subject;
+    delete Student.marks;
+    this.excluded = reason;
+}
